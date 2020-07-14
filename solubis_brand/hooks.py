50add5390bce448a5003fb2fa9021298d4342b8e
bobzz-zone <bobzz.zone@gmail.com>
2020-07-14 18:11:00 +0700

    Update hooks.py

--- solubis_brand/hooks.py
+++ solubis_brand/hooks.py
@@ -91,9 +91,9 @@ doc_events = {
 		# "on_submit" : my_account.doctype.sync_server_settings.create_new_user
 	}
 }
-on_session_creation =[
-	"solubis_brand.custom_function.login_block"
-]
+# on_session_creation =[
+# 	"solubis_brand.custom_function.login_block"
+# ]
 # Scheduled Tasks
 # ---------------
 
