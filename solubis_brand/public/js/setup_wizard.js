9020427503a8b3342d848cbf1ba6abda88f14d0d
bobzz-zone <bobzz.zone@gmail.com>
2020-07-16 01:21:12 +0700

    Update setup_wizard.js

--- solubis_brand/public/js/setup_wizard.js
+++ solubis_brand/public/js/setup_wizard.js
@@ -5,11 +5,8 @@ frappe.setup.on("before_load", function () {
 	frappe.setup.remove_slide('user');
 	//erpnext.setup.slides_settings.map(frappe.setup.add_slide);
 });
-/*frappe.setup.on("onload", function () {
+frappe.setup.on("after_load", function () {
 	frappe.setup.domains="Retail"
+	frappe.wizard.values['domains']="Retail"
 	
-});*/
-
-frappe.pages['setup-wizard'].on_page_load = function(wrapper) {
-	frappe.setup.domains="Retail"
-};
+});
