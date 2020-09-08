b2025903cb0dd4ea9f8e4975b24a671a5e0440c8
bobzz-zone <bobzz.zone@gmail.com>
2020-09-08 15:59:56 +0700

    Update pos_api.py

--- solubis_brand/pos/pos_api.py
+++ solubis_brand/pos/pos_api.py
@@ -62,7 +62,8 @@ def get_pos_profile(user):
 @frappe.whitelist()
 def get_customer(keyword):
 	keyword="%{}%".format(keyword)
-	return frappe.db.sql("""select name , customer_group , territory , customer_name , mobile_no , primary_address ,email_id from `tabCustomer` where customer_name like "{0}" or name like "{0}" or mobile_no like "{0}" or primary_address like "{0}" """.format(keyword), as_dict=1)
+	company = frappe.get_doc("Company",get_default_company())
+	return frappe.db.sql("""select name , customer_group , territory , customer_name , mobile_no , primary_address ,email_id, "{1}" as default_piutang from `tabCustomer` where customer_name like "{0}" or name like "{0}" or mobile_no like "{0}" or primary_address like "{0}" """.format(keyword,company.default_receivable_account), as_dict=1)
 def master_get_item_details(warehouse,customer, transaction_date,doc_item):
 	warehouse = get_default_warehouse(warehouse)
 	price_list = get_default_price_list(customer)
